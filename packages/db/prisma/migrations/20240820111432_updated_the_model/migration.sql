9b95d69e34507212254f8686ca1f3494b2c66ec9
imkurosaki <keandelaserna11@gmail.com>
2024-08-21 09:37:44 +0800

    Minor modification

---
+++ packages/db/prisma/migrations/20240820111432_updated_the_model/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - Made the column `firstName` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `lastName` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropIndex
+DROP INDEX "User_username_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "username",
+ALTER COLUMN "firstName" SET NOT NULL,
+ALTER COLUMN "lastName" SET NOT NULL;
