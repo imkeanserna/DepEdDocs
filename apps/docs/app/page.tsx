34f2416e970d2898a345e91bffec1952b160897b
imkurosaki <keandelaserna11@gmail.com>
2024-08-21 08:57:15 +0800

    Testing for deploying

--- apps/docs/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Dashboard } from "../screens/Dashboard";
-
-export default function Home() {
-  return <div>
-    <Dashboard />
-  </div>
-}
--- apps/docs/components/demo/placeholder-content.tsx
+++ apps/docs/components/demo/placeholder-content.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 
-import { Card, CardContent } from "@repo/ui/components/ui/card";
+import { CardContent } from "@repo/ui/components/ui/card";
 import ClientTable from "~/app/users/client-table";
 
 export default function PlaceholderContent() {
